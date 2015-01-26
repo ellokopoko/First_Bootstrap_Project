2ef44c78b2173c23c91e81928510874068cc8906
Mike <ja-morning@yandex.ua>
2015-01-26 12:35:21 +0400

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,174 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+	concat: {
+	    options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+       },
+	    bootstrap: { // Обязательно сохраняйте порядок склеки
+	        src: ['_/components/javascripts/jquery.js', // JQuery
+				'_/components/javascripts/affix.js', // Start bootstrap
+				'_/components/javascripts/transition.js',
+				'_/components/javascripts/tooltip.js',
+				'_/components/javascripts/alert.js',
+				'_/components/javascripts/button.js',
+				'_/components/javascripts/carousel.js',
+				'_/components/javascripts/collapse.js',
+				'_/components/javascripts/dropdown.js',
+				'_/components/javascripts/madal.js',
+				'_/components/javascripts/popover.js',
+				'_/components/javascripts/scrollspy.js',
+				'_/components/javascripts/tab.js' // End Botstrap
+				],
+            dest: '_/components/javascripts/_bootstrap.js'
+	    },
+        all_js: {
+            src: [
+                '_/components/javascripts/_bootstrap.js',
+                '_/components/javascripts/my_working.js' // My js compiled from coffee script
+            ],
+            dest: '_/components/javascripts/_main.js'
+        },
+	    all_css: {
+	        src: ['_/components/styles/bootstrap.css',
+                  '_/components/styles/less.css',
+                  '_/components/styles/sass.css'
+                 ],
+            dest: '_/components/styles/_style.css'
+	    }
+	},
+    uglify: {
+        options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
+        build: {
+            src: '_/components/javascripts/_main.js',
+            dest: '_/javascripts/main.min.js'
+        }
+    },
+    cssmin: {
+        build: {
+            src: '_/components/styles/_style.css',
+            dest: '_/styles/style.min.css'
+        }
+    },
+	coffee: {
+		options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
+        build: {
+            src: '_/components/javascripts/working/my_working.coffee',
+            dest: '_/components/javascripts/my_working.js'
+        }
+	},
+	haml: {
+		options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+			language: 'coffee'
+        },
+        index: {
+            src: '_/components/views/index.haml',
+            dest: 'index.php'
+        },
+        about: {
+            src: '_/components/views/about.haml',
+            dest: 'about.php'
+        },
+        artists: {
+            src: '_/components/views/artists.haml',
+            dest: 'artists.php'
+        },
+        privacypolicy: {
+            src: '_/components/views/privacypolicy.haml',
+            dest: 'privacypolicy.php'
+        },
+        register: {
+            src: '_/components/views/register.haml',
+            dest: 'register.php'
+        },
+        schedule: {
+            src: '_/components/views/schedule.haml',
+            dest: 'schedule.php'
+        },
+        venuetravel: {
+            src: '_/components/views/venuetravel.haml',
+            dest: 'venuetravel.php'
+        },
+        header: {
+            src: '_/components/views/layouts/header.haml',
+            dest: '_/views/layouts/header.php'
+        },
+        footer: {
+            src: '_/components/views/layouts/footer.haml',
+            dest: '_/views/layouts/footer.php'
+        }
+	},
+	less: {
+		options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        },
+        build: {
+            src: '_/components/styles/working/less.less',
+            dest: '_/components/styles/less.css'
+        },
+		bootstrap: {
+			src: '_/components/styles/bootstrap3/bootstrap.less',
+            dest: '_/components/styles/bootstrap.css'
+		}
+	},
+	sass: {
+		options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+			compass: true,
+			style: 'expanded',
+			debugInfo: false
+        },
+		build: {
+            src: '_/components/styles/working/sass.sass',
+            dest: '_/components/styles/sass.css'
+        }
+	},
+	compass: {
+		options: {
+            sassDir: '_/components/styles',
+			cssDir: '_/css'
+        }
+	},
+	watch: { // grunt watch для запуска отслеживания
+		views: {
+			files: "_/components/views/*.haml",
+			tasks: 'haml'
+		},
+        layouts: {
+            files: "_/components/views/layouts/*.haml",
+			tasks: 'haml'
+        },
+        styles: {
+            files: "_/components/styles/working/*.sass",
+			tasks: ['sass', 'concat:all_css', 'cssmin']
+        },
+        scripts: {
+            files: "_/components/javascripts/working/*.coffee",
+			tasks: ['coffee', 'concat:all_js', 'uglify']
+        }
+	}
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  //require('load-grunt-tasks')(grunt);
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-haml');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'coffee', 'haml', 'less', 'sass', 'compass', 'watch']);
+
+};
---
+++ _/views/venuetravel.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html lang='ru'>
+  <head>
+    <title>Место путеществия</title>
+    <meta content='text/html' charset='utf-8'>
+    <link href='http://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,700italic,300,300italic,400italic,700' rel='stylesheet' type='text/css'>
+    <link href='_/styles/style.min.css' rel='stylesheet' type='text/css'>
+    <script src='_/javascripts/main.min.js' type='text/javascript'></script>
+  </head>
+  <body id='venuetravel'>
+    <section class='container'>
+      <div class='content row'>
+        <section class='col col-lg-8 main'>
+          Main content
+        </section>
+        <section class='col col-lg-4 sidebar'>
+          Sidebar
+        </section>
+      </div>
+    </section>
+    <!-- *
+      <?php include "_/views/layouts/header.php"; ?>
+      <?php include "_/views/layouts/footer.php"; ?>
+     -->
+  </body>
+</html>
